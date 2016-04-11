9a2b79268ff0249f27b6efbe2dff7d8f9e7857ac
barbara fux <barbara.fux@gmx.at>
2016-04-11 17:28:21 +0200

    added solution

--- learntoprogram/chapter2/minutesinadecade.rb
+++ learntoprogram/chapter2/minutesinadecade.rb
@@ -1,1 +1,12 @@
 # Minutes in a decade. How many minutes are in a decade?
+
+minutes_year = 365 * 24 * 60
+minutes_leap_year = 366 * 24 * 60
+
+# if there are 2 leap years in that decade
+minutes_decade = minutes_year * 8 + minutes_leap_year * 2
+
+# if there are 3 leap years in that decade
+# minutes_decade = minutes_year * 7 + minutes_leap_year * 3
+
+puts "There are #{minutes_decade} minutes in a decade."
