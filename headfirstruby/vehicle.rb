f94f1e1841246ad557177fba32752fd09c1615d8
barbara fux <barbara.fux@gmx.at>
2016-04-29 20:48:10 +0200

    exercise to learn more about methods

---
+++ headfirstruby/vehicle.rb
@@ -0,0 +1,20 @@
+def accelerate
+  puts "Stepping on the gas."
+  puts "Speeding up."
+end
+
+def sound_horn
+  puts "Pressing the horn button."
+  puts "Beep, beep."
+end
+
+def use_headlights(brightness = "low-beam")
+  puts "Turning on #{brightness} headlight."
+  puts "Watch out for deer."
+end
+
+
+# accelerate
+# sound_horn
+# use_headlights
+# use_headlights("high-beam")
