e74d7c79a36594d028b90eedc3b4945e250f2649
barbara fux <barbara.fux@gmx.at>
2016-04-29 21:42:23 +0200

    learning about classes

---
+++ headfirstruby/animals.rb
@@ -0,0 +1,42 @@
+class Bird
+
+  def talk
+    puts "Chirp! Chirp!"
+  end
+
+  def move(destination)
+    puts "Flying to the #{destination}."
+  end
+
+end
+
+class Dog
+
+  def talk
+    puts "Bark!"
+  end
+
+  def move(destination)
+    puts "Running to the #{destination}."
+  end
+
+end
+
+class Cat
+
+  def talk
+    puts "Meow!"
+  end
+
+  def move(destination)
+    puts "Running to the #{destination}."
+  end
+
+end
+
+fido = Dog.new
+fido.talk
+
+sircatalot = Cat.new
+sircatalot.talk
+sircatalot.move("Tree")
