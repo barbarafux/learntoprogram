c080fcdde1425b85d8be86eba16758118df469cf
barbara fux <barbara.fux@gmx.at>
2016-08-23 17:17:23 +0200

    added advanced payroll example

---
+++ headfirstruby/chapter4/payroll_extended.rb
@@ -0,0 +1,93 @@
+class Employee
+  attr_reader :name, :salary
+
+  def initialize(name = "Anonymous")
+    self.name = name
+  end
+
+  def name=(name)
+    if name == ""
+      raise "Name can't be blank."
+    end
+    @name = name
+  end
+
+  def print_name
+    puts "Name: #{name}"
+  end
+end
+
+class SalariedEmployee < Employee
+  attr_reader :salary
+
+  def initialize(name = "Anonymous", salary = 0.0)
+    super(name)
+    self.salary = salary
+  end
+
+  def salary=(salary)
+    if salary <= 0
+      raise "Salary can't be #{salary}."
+    end
+    @salary = salary
+  end
+
+  def print_pay_stub
+    print_name
+    pay_per_period = (salary/365.0) * 14.0
+    formatted_pay = format("%0.2f", pay_per_period)
+    puts "The pay for the period is #{formatted_pay}."
+  end
+end
+
+class HourlyEmployee < Employee
+  attr_reader :hourly_wage, :hours_per_week
+
+  def initialize(name = "Anonymous", hourly_wage = 0.0, hours_per_week = 0.0)
+    super(name)
+    self.hourly_wage = hourly_wage
+    self.hours_per_week = hours_per_week
+  end
+
+  def hourly_wage=(hourly_wage)
+    if hourly_wage < 0
+      raise "Hourly wage can't be #{hourly_wage}."
+    end
+    @hourly_wage = hourly_wage
+  end
+
+  def hours_per_week=(hours_per_week)
+    if hours_per_week < 0
+      raise "Hours per week can't be #{hours_per_week}"
+    end
+    @hours_per_week = hours_per_week
+  end
+
+  def print_pay_stub
+    print_name
+
+    pay_per_period = (hourly_wage * hours_per_week) * 2
+    formatted_pay = format("%0.2f", pay_per_period)
+    puts "The pay for the period is #{formatted_pay}."
+  end
+
+end
+
+
+# salariedEmployee = SalariedEmployee.new
+# salariedEmployee.name = "Kara"
+# salariedEmployee.salary = 50000
+# salariedEmployee.print_pay_stub
+
+# hourlyEmployee = HourlyEmployee.new
+# hourlyEmployee.name = "John Smith"
+# hourlyEmployee.hourly_wage = 14.97
+# hourlyEmployee.hours_per_week = 30
+# hourlyEmployee.print_pay_stub
+#
+salariedEmployee = SalariedEmployee.new("Jane Doe", 50000)
+salariedEmployee.print_pay_stub
+
+hourlyEmployee = HourlyEmployee.new("John Smith", 14.97, 30)
+hourlyEmployee.print_pay_stub
+
