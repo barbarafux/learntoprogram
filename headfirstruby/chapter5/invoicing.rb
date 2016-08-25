cf9055a4688a425f78aa1191e5f35b04953e83bd
barbara fux <barbara.fux@gmx.at>
2016-08-25 13:27:31 +0200

    refactored methods using blocks

--- headfirstruby/chapter5/invoicing.rb
+++ headfirstruby/chapter5/invoicing.rb
@@ -1,44 +1,35 @@
 def total(prices)
   amount = 0
-  index = 0
 
-  while index < prices.length
-    amount += prices[index]
-    index +=1
+  prices.each  do |price|
+    amount += price
   end
   formatted_amount = format("%0.2f", amount)
   puts formatted_amount
 end
 
-
 def refund(prices)
   amount = 0
-  index = 0
 
-  while index < prices.length
-    amount -= prices[index]
-    index +=1
+  prices.each do |price|
+    amount -= price
   end
   formatted_amount = format("%0.2f", amount)
   puts formatted_amount
 end
 
 def show_discount(prices)
-  index = 0
-  while index < prices.length
-    amount_off = prices[index] / 3
-
+  prices.each do |price|
+    amount_off = price/ 3.0
     puts format("Your discount: $%0.2f", amount_off)
-    index += 1
   end
-
 end
 
-
-prices =  [3.99, 25.00, 8.99]
-# total(prices)
-# refund(prices)
-show_discount(prices)
-
-
+#prices =  [3.99, 25.00, 8.99]
+#total(prices)
+#refund(prices)
+#show_discount(prices)
+total([3.99, 25.00, 8.99])
+refund([3.99, 25.00, 8.99])
+show_discount([3.99, 25.00, 8.99])
 
