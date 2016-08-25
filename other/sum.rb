8d2c088b1b2ef8686f27811602db68d4cf44086d
barbara fux <barbara.fux@gmx.at>
2016-08-25 16:09:26 +0200

    exercise to calculate sum of numbers 1 to n

---
+++ other/sum.rb
@@ -0,0 +1,9 @@
+# Asks for a number n and calculate the sum of the numbers 1 to n
+
+puts "Enter a number"
+input = gets.chomp.to_i
+
+sum = 0
+
+(1..input).each {|num| sum += num }
+  puts sum.to_i
