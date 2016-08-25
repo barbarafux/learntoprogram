dd819343d23256b85adb1ed3633b851699be8276
barbara fux <barbara.fux@gmx.at>
2016-08-25 19:57:38 +0200

    added find_all method to find keywords

--- headfirstruby/chapter6/film_critic.rb
+++ headfirstruby/chapter6/film_critic.rb
@@ -4,5 +4,8 @@ File.open("reviews.txt") do |review_file|
  lines = review_file.readlines
 end
 
-puts lines[3]
-puts lines[0]
+#puts lines[3]
+#puts lines[0]
+
+relevant_lines = lines.find_all{ |line| line.include?("Truncated")}
+puts relevant_lines
