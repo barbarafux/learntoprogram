a9a9d483271b006f0c8e78c3753bc1f67b85eb9d
barbara fux <barbara.fux@gmx.at>
2016-08-26 18:46:03 +0200

    find adjectives in reviews

--- headfirstruby/chapter6/film_critic.rb
+++ headfirstruby/chapter6/film_critic.rb
@@ -8,4 +8,25 @@ end
 #puts lines[0]
 
 relevant_lines = lines.find_all{ |line| line.include?("Truncated")}
-puts relevant_lines
+#puts relevant_lines
+
+reviews = relevant_lines.reject { |line| line.include?("--")}
+
+#p reviews
+
+def find_adjectives(string)
+  words = string.split(" ")
+  index = words.find_index("is")
+  words[index + 1]
+end
+
+#adjective = find_adjectives(reviews.first)
+#puts adjective
+
+adjectives = []
+
+reviews.each do |review|
+  adjectives << find_adjectives(review)
+end
+
+puts adjectives
